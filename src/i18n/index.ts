d7b8fcb7c0eca2d452e77e13ce403c5a3b1eb400
djurayevdavron <djurayevdavron7@gmail.com>
2026-06-29 12:21:35 +0500

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,28 +1,37 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import uz from "./locales/uz.json";
 import en from "./locales/en.json";
 import ru from "./locales/ru.json";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    uz: {
-      translation: uz,
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      uz: {
+        translation: uz,
+      },
+      en: {
+        translation: en,
+      },
+      ru: {
+        translation: ru,
+      },
     },
-    en: {
-      translation: en,
-    },
-    ru: {
-      translation: ru,
-    },
-  },
 
-  lng: "en",
-  fallbackLng: "en",
+    fallbackLng: "en",
 
-  interpolation: {
-    escapeValue: false,
-  },
-});
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
 export default i18n;
